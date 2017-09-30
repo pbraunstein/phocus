d30455e7a6f6c8e013b8f90c662660747cbe6b0a
pbraunstein <pbraunstein12@gmail.com>
2017-09-30 15:30:24 -0400

    button responds to event

---
+++ popup.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("I am here")
+    var button = document.getElementById('alertButton');
+    button.addEventListener('click', function () {
+        alert("Hello! This looks like a distracting webpage");
+    });
+});
+
+
+console.log("HIHIHIHIIHIHIHIh");
+
